de36fb8ec88610fecbaaa376caf60cff8a94f9c1
takaaki-s <5150anpan@gmail.com>
2021-09-14 22:19:30 +0900

    commit

--- .zsh/rc/config.zsh
+++ .zsh/rc/config.zsh
@@ -3,3 +3,11 @@ export FZF_ALT_C_COMMAND='fd --type d'
 
 export FZF_CTRL_T_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 export FZF_CTRL_T_OPTS='--preview "bat  --color=always --style=header,grid --line-range :100 {}"'
+
+# Solarized colors
+export FZF_DEFAULT_OPTS='
+--color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+--color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
+'
+export FZF_TMUX=1
+
--- .zsh/rc/function.zsh
+++ .zsh/rc/function.zsh
@@ -1,5 +1,5 @@
 function zz() {
-  read -r recent < <(z -l | fzf -q "$argv" | awk '{ print $2 }')
+  read -r recent < <(z -l | fzf-tmux -q "$argv" | awk '{ print $2 }')
   if [ "$recent" ]; then
       cd "$recent" || return
   fi
