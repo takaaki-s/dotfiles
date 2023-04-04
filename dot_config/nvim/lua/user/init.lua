718789bcff837d59681d91c75df9d5ff568a9283
takaaki-freee <takaaki-sato@c-fo.com>
2023-04-04 11:10:23 +0900

    commit

--- dot_config/nvim/lua/user/init.lua
+++ dot_config/nvim/lua/user/init.lua
@@ -1,17 +1,32 @@
+if vim.env.TMUX then
+	vim.g.clipboard = {
+		name = "tmux",
+		copy = {
+			["+"] = { "tmux", "load-buffer", "-w", "-" },
+			["*"] = { "tmux", "load-buffer", "-w", "-" },
+		},
+		paste = {
+			["+"] = { "tmux", "save-buffer", "-" },
+			["*"] = { "tmux", "save-buffer", "-" },
+		},
+		cache_enabled = false,
+	}
+end
+
 return {
-  colorscheme = "onedark",
+	colorscheme = "onedark",
 
-  options = {
-    g = {
-      icons_enabled = false,
-    },
-  },
+	options = {
+		g = {
+			icons_enabled = false,
+		},
+	},
 
-  lsp = {
-    formatting = {
-      disabled = {
-        "lua_ls",
-      },
-    },
-  },
+	lsp = {
+		formatting = {
+			disabled = {
+				"lua_ls",
+			},
+		},
+	},
 }
